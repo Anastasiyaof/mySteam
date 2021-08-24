6a59a592ff1f22f38db62318ef7098fe3d567107
Anasyasiia Fomenko <anastasiyaof@gmail.com>
2021-08-24 20:48:10 +0300

    server

--- server.js
+++ server.js
@@ -2,15 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const { gamesController } = require('./src/app/controllers/games.controller')
-
 
 app.use(express.static(__dirname + '/dist/mySteam'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/itea-project/index.html'));
 });
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mySteam/index.html'));
-})
 app.listen(process.env.PORT || 8080);
